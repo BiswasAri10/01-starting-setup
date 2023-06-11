588fe647771c8f2cb176a0fa522243b2baccbedd
BiswasAri10 <bis.ips21@gmail.com>
2023-06-11 13:14:30 +0530

    Expense Tracker React JS

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,12 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>March 15th 2023</div>
+      <div>
+        <h2>Bike Insurance</h2>
+        <div>Rs. 1650</div>
+      </div>
+    </div>
+  );
+}
+export default ExpenseItem;
