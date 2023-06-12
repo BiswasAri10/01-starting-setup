8f55a70c5d00d541fece0e94e938c67520e14fd3
BiswasAri10 <bis.ips21@gmail.com>
2023-06-12 13:06:39 +0530

    props added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    { id: 'e1', title: "Bike Insurance", amount: 1650, date: new Date(2023, 2, 15), location: "Acko Insurance" },
+    { id: 'e2', title: "Iphone", amount: 36500, date: new Date(2023, 3, 2), location: "A.S Telecom" },
+    { id: 'e3', title: "TV", amount: 19999, date: new Date(2023, 4, 4), location: "Flipkart" },
+    { id: 'e4', title: "Laptop", amount: 24500, date: new Date(2023, 5, 29), location: "Chadni Chowk" },
+  ];
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      {expenses.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+          location={expense.location}
+        />
+      ))}
     </div>
   );
 }
