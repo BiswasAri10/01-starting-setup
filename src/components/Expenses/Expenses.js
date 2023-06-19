1bd2083396e4d1d8f865aeb7001bd46bbe3dffdd
BiswasAri10 <bis.ips21@gmail.com>
2023-06-19 16:45:10 +0530

    Filter functionality added

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -8,14 +8,20 @@ import './Expenses.css';
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2023');
+  const [filteredExpenses, setFilteredExpenses] = useState(props.items);
+
 
   const filterChangeHandler = selectedYear => {
     setFilteredYear(selectedYear);
+    const filteredExpenses = props.items.filter(expense => {
+      return expense.date.getFullYear().toString() === selectedYear;
+    });
+    setFilteredExpenses(filteredExpenses);
   };
   return (
     <Card className="expenses">
       <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-      {props.items.map((expense) => (
+      {filteredExpenses.map((expense) => (
         <ExpenseItem
           key={expense.id}
           title={expense.title}
